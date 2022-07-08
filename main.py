4ea98813e82228fb958dadcd1a8fd43e8c3fd244
Katsutami7moto <nclsf87@gmail.com>
2022-07-09 01:09:32 +0300

    Handle connection failure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from time import sleep
 
 import requests
 from environs import Env
@@ -13,18 +14,30 @@ def main():
         'Authorization': f'Token {token}',
     }
 
+    first_reconnection = True
     while True:
         try:
             response = requests.get(
                 long_poll_url,
                 headers=headers,
-                timeout=5
+                timeout=60
             )
             response.raise_for_status()
-            status = response.json()
+            status: dict = response.json()
             print(json.dumps(status, indent=4, ensure_ascii=False))
+        except requests.exceptions.ConnectionError as connect_err:
+            print(f'Connection failure: {connect_err};')
+            if first_reconnection:
+                print('Retry in 5 seconds')
+                sleep(5)
+                first_reconnection = False
+            else:
+                print('Retry in 15 seconds')
+                sleep(15)
         except requests.exceptions.ReadTimeout:
             continue
+        else:
+            first_reconnection = True
 
 
 if __name__ == "__main__":
