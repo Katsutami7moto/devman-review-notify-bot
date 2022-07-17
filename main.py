fd801964fb73875e9c7ae42f023683a75ec034d7
Katsutami7moto <nclsf87@gmail.com>
2022-07-17 05:46:49 +0300

    Decompose errors handling

--- main.py
+++ main.py
@@ -24,42 +24,16 @@ def send_notification(bot: Bot, chat_id: int, attempt: dict):
     )
 
 
-def get_reviews(devman_token: str, bot: Bot, tg_chat_id: int):
-    long_poll_url = 'https://dvmn.org/api/long_polling/'
-    headers = {
-        'Authorization': f'Token {devman_token}',
-    }
+def handle_errors(func_, *args):
     first_reconnection = True
     timestamp = time()
     while True:
         try:
-            payload = {
-                'timestamp': timestamp
-            }
-            response = requests.get(
-                long_poll_url,
-                headers=headers,
-                params=payload,
-                timeout=120
-            )
-            response.raise_for_status()
-            lessons_reviews: dict = response.json()
-            devman_server_status = lessons_reviews.get('status')
+            timestamp = func_(timestamp, *args)
 
             if not first_reconnection:
                 logging.warning('Connection is restored.')
                 first_reconnection = True
-
-            if devman_server_status == 'timeout':
-                timestamp = lessons_reviews.get('timestamp_to_request')
-                continue
-            elif devman_server_status == 'found':
-                timestamp = lessons_reviews['last_attempt_timestamp']
-                new_attempts: list[dict] = lessons_reviews['new_attempts']
-                for attempt in new_attempts:
-                    send_notification(bot, tg_chat_id, attempt)
-            else:
-                logging.warning(response)
         except requests.exceptions.ConnectionError as connect_err:
             logging.exception(connect_err)
             if first_reconnection:
@@ -73,6 +47,33 @@ def get_reviews(devman_token: str, bot: Bot, tg_chat_id: int):
             logging.exception(err)
 
 
+def get_reviews(timestamp: float, bot: Bot, tg_chat_id: int,
+                long_poll_url: str, headers: dict) -> float:
+    payload = {
+        'timestamp': timestamp
+    }
+    response = requests.get(
+        long_poll_url,
+        headers=headers,
+        params=payload,
+        timeout=120
+    )
+    response.raise_for_status()
+    lessons_reviews: dict = response.json()
+    devman_server_status = lessons_reviews.get('status')
+
+    if devman_server_status == 'timeout':
+        timestamp = lessons_reviews.get('timestamp_to_request')
+    elif devman_server_status == 'found':
+        timestamp = lessons_reviews['last_attempt_timestamp']
+        new_attempts: list[dict] = lessons_reviews['new_attempts']
+        for attempt in new_attempts:
+            send_notification(bot, tg_chat_id, attempt)
+    else:
+        logging.warning(response)
+    return timestamp
+
+
 def main():
     env = Env()
     env.read_env()
@@ -83,7 +84,19 @@ def main():
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
     )
-    get_reviews(devman_token, bot, tg_chat_id)
+
+    long_poll_url = 'https://dvmn.org/api/long_polling/'
+    headers = {
+        'Authorization': f'Token {devman_token}',
+    }
+
+    handle_errors(
+        get_reviews,
+        bot,
+        tg_chat_id,
+        long_poll_url,
+        headers
+    )
 
 
 if __name__ == "__main__":
